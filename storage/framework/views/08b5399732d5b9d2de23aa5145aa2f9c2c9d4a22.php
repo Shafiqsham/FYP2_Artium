76e42b162f5360c08fb9a9face0b3661dd027be8
Shafiqsham <55779791+Shafiqsham@users.noreply.github.com>
2021-08-12 09:46:56 +0800

    Add files via upload

---
+++ storage/framework/views/08b5399732d5b9d2de23aa5145aa2f9c2c9d4a22.php
@@ -0,0 +1,7 @@
+<br><br><br><br><br>
+<div class="panel panel-default">
+  <div class="panel-body">
+    Panel content
+  </div>
+  <div class="panel-footer">Panel footer</div>
+</div><?php /**PATH C:\Users\Muhammad Shafiq\fyp2\resources\views/footer.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/d7e5cc61824f4d723a9b6ec82964cc27f4135dd1.php
@@ -0,0 +1,29 @@
+;
+<?php $__env->startSection("content"); ?>
+<div class="container custom-login">
+    <div class="row">
+        <div class="col-sm-4 col-sm-offset-4">
+            <h1>Welcome to Artium !</h1>
+            <form action="login" method="POST">
+  <div class="form-group">
+    <?php echo csrf_field(); ?>
+    <label for="Username">Username</label>
+    <input type="AccName" name="Username" class="form-control" id="Username" placeholder="Username">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="Password" name="Password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="checkbox">
+    <label>
+      <input type="checkbox"> Remember me
+    </label>
+  </div>
+  <br><button type="CustLogin" class="btn btn-primary">Log in</button>
+</form>
+<br><br><button>Create New Account</button>
+</div>
+</div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Muhammad Shafiq\fyp2\resources\views/login.blade.php ENDPATH**/ ?>
